0c7b0f10188b89ae474fac5921b5e956f7192b62
Matheus Duca <63525232+sktheu@users.noreply.github.com>
2023-10-06 18:22:16 -0300

    Progresso mochila III

--- Smaug4/Assets/_Game/_Scripts/Entities/Objectives/TreasureBehaviour.cs
+++ Smaug4/Assets/_Game/_Scripts/Entities/Objectives/TreasureBehaviour.cs
@@ -4,15 +4,44 @@ using UnityEngine;
 
 public class TreasureBehaviour : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [Header("Configurações:")] 
+    [SerializeField] private float verticalSpeed;
+    [SerializeField] private float collisionSpeed;
+    [SerializeField] private float verticalDirInterval;
+
+    [Header("Sprites:")] 
+    [SerializeField] private Sprite[] sprites;
+
+    // Componentes:
+    private Rigidbody2D _rb;
+
+    // Movimento Vertical:
+    private float _dirY = 1f;
+    private bool _canMoveY = true;
+
+    private void Awake() => GetComponent<SpriteRenderer>().sprite = sprites[Random.Range(0, sprites.Length)];
+
+    private void Start()
     {
-        
+        _rb = GetComponent<Rigidbody2D>();
+        StartCoroutine(FlipDirY(verticalDirInterval));
     }
 
-    // Update is called once per frame
-    void Update()
+    private void FixedUpdate()
     {
-        
+        if (_canMoveY)
+            ApplyVerticalMove();
+    }
+
+    private void ApplyVerticalMove() => _rb.velocity = Vector2.up * _dirY * verticalSpeed;
+
+    private IEnumerator FlipDirY(float t)
+    {
+        yield return new WaitForSeconds(t);
+        if (_canMoveY)
+        {
+            _dirY *= -1f;
+            StartCoroutine(FlipDirY(verticalDirInterval));
+        }
     }
 }
