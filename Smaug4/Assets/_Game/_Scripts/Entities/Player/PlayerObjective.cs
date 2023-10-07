f6274f51d8807845391a683d1bfcd8fbb3ce807c
Matheus Duca <63525232+sktheu@users.noreply.github.com>
2023-10-06 21:26:03 -0300

    Progresso mochila VI

--- Smaug4/Assets/_Game/_Scripts/Entities/Player/PlayerObjective.cs
+++ Smaug4/Assets/_Game/_Scripts/Entities/Player/PlayerObjective.cs
@@ -1,5 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime;
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -12,17 +14,21 @@ public class PlayerObjective : MonoBehaviour
     // Tesouros coletados:
     private List<Sprite> _currentTreasureSprites = new List<Sprite>();
     private int _treasuresLeft;
+
+    // Mochila:
+    private static Animator _backpackAnimator;
+    private static TextMeshPro _backpackText;
     #endregion
 
     #region Funções Unity
-
     private void Awake()
     {
         _collisionLayersManager = GameObject.FindObjectOfType<CollisionLayersManager>();
         _treasuresLeft = GameObject.FindObjectsOfType<TreasureBehaviour>().Length;
+        _backpackAnimator = GameObject.FindGameObjectWithTag("Backpack").GetComponent<Animator>();
     }
 
-    private void OnTriggerStay2D(Collider2D col)
+    private void OnTriggerEnter2D(Collider2D col)
     {
         if (_currentTreasureSprites.Count > 0 && col.gameObject.layer == _collisionLayersManager.Backpack.Index)
             RemoveTreasure();
@@ -37,6 +43,7 @@ public class PlayerObjective : MonoBehaviour
         _currentTreasureSprites.RemoveAt(0);
         _treasuresLeft--;
 
+        _backpackAnimator.Play("BackpackDeposit");
         if (_treasuresLeft <= 0)
             SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }   
